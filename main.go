22341f489a51bac0ff900cde1906b31b9fbec5c1
Takumi Yasufuku <run.taku12@gmail.com>
2024-01-16 23:03:32 +0900

    config

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"adform-go/config"
 	"adform-go/server"
+	"flag"
 )
 
 func main() {
+	env := flag.String("e", "development","")
+	flag.Parse()
+
+	config.Init(*env)
 	if err := server.Init(); err != nil {
 		panic(err)
 	} 
